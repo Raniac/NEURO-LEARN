b431728b9cfbc1b2e052d3a7376d2e791c68ace7
raniac <leibingye@outlook.com>
2019-12-17 18:45:33 +0800

    Modified update_docker.

--- backend/tools/update_docker.sh
+++ backend/tools/update_docker.sh
@@ -1,9 +1,9 @@
-# update backend modules
+## update backend modules ##
 cp /home/bennyray/Projects/neuro-learn/web/neurolearn_dev/backend/views.py /home/bennyray/Projects/neuro-learn/docker/dev/app/backend/
 
-# update frontend static
-rm -rf /home/bennyray/Projects/neuro-learn/docker/dev/app/frontend/dist/
-cp -r /home/bennyray/Projects/neuro-learn/web/neurolearn_dev/frontend/dist/ /home/bennyray/Projects/neuro-learn/docker/dev/app/frontend/
-source /home/bennyray/Projects/neuro-learn/web/neurolearn_dev/env/bin/activate
-cd /home/bennyray/Projects/neuro-learn/docker/dev/app/
-python manage.py collectstatic
+## update frontend static ##
+# rm -rf /home/bennyray/Projects/neuro-learn/docker/dev/app/frontend/dist/
+# cp -r /home/bennyray/Projects/neuro-learn/web/neurolearn_dev/frontend/dist/ /home/bennyray/Projects/neuro-learn/docker/dev/app/frontend/
+# source /home/bennyray/Projects/neuro-learn/web/neurolearn_dev/env/bin/activate
+# cd /home/bennyray/Projects/neuro-learn/docker/dev/app/
+# python manage.py collectstatic
